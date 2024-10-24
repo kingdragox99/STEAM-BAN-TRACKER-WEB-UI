e267f31efa400bfa423ca4c66707667ac5ca483b
Arthur Lefevre <kingdragox99@gmail.com>
2024-10-24 17:05:46 +0200

    feat: better structure

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer footer-center bg-base-100 text-white p-4">
+    <aside>
+      <p>Made by Dr. Agorille</p>
+    </aside>
+  </footer>
+</template>
---
+++ components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="footer footer-center bg-base-100 text-white p-2">
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">Steam Ban Tracker Web</a>
+      </div>
+    </div>
+  </header>
+</template>
